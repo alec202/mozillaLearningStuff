b40a7275f9d1aa315f254cde125dde12b45c10c5
Alec Mirambeau <alecmirambeau@gmail.com>
2025-01-02 21:47:24 -0500

    moved stuff into this repository

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,46 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/library-three-pane-window.jpeg") {
+    myImage.setAttribute("src", "images/lots-of-shelves-library.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/library-three-pane-window.jpeg");
+  }
+};
+
+
+// Start of code to assign user name
+let myButton = document.querySelector("button")
+let myHeading = document.querySelector("h1")
+
+// function to set the user name
+function setUserName(){
+    const myName = prompt("Please enter your name.");
+    if (myName == " " || myName === " " || !myName){
+        // recurse on this function until myName has a value. Next steps would be ensuring at least one of the characters is a letter character.
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Welcome to Miriam's library, ${myName}`;
+        }
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
+// End of code to assign user name
+
+
+// Function to initialize the website
+function loadPreviousNameIfSaved(){
+    console.log(`stored user name value is: ${localStorage.getItem("name")}!`)
+    if (!localStorage.getItem("name")){
+        setUserName();
+    } else {
+        const storedName = localStorage.getItem("name");
+        myHeading.textContent = `Welcome to Miriam's library, ${storedName}`;
+    }
+}
+
+loadPreviousNameIfSaved();
---
+++ websiteIdeas/general_website_idea_brainstorm.md
@@ -0,0 +1,20 @@
+The website I intend on designing will be the library database one
+==================================================================
+
+# Library Database Management System with a SQL/Cloud database component.
+* Tables on:
+  - Books
+  - Customers Loyalty/Rewards
+  - Employee Information
+  - Purchases/Receipts table 
+* Perform joins to view which employees are rewards members.
+* Perform joins based off customer unique ID with purchases/receipts table to view their recent purchases.
+
+# To Do List Website #
+* Login screen with cloud component to maintain to do list data.
+  - Maintain completed items in cloud
+* On item completion add celebration animation
+
+# Weather Website through API with user location tracking
+* tracks users location
+* Could maintain a database with user's visited locations whether that's cloud based or not
