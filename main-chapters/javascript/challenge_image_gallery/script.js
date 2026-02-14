0bac8d36704f8f5d7248211cc9a42e275b8197e0
Alec Mirambeau <alecmirambeau@gmail.com>
2026-02-14 17:10:19 -0500

    on the img thumbnail bar

--- main-chapters/javascript/challenge_image_gallery/script.js
+++ main-chapters/javascript/challenge_image_gallery/script.js
@@ -4,3 +4,32 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+class image {
+    constructor(fileName, alt) {
+        this.fileName = fileName;
+        this.alt = alt;
+    }
+}
+
+const images = [
+    new image("pic1_an_eye", "Closeup of a human eye"),
+    new image("pic2_rock_looks_like_wave", "Rock that looks like a wave"),
+    new image("pic3_purple_pansies", "Purple and white pansies"),
+    new image("pic4_pharaohs_tomb", "Section of wall from a pharaoh's tomb"),
+    new image("pic5_moth", "Large moth on a leaf")
+];
+// array of images done
+
+const baseURL = "./images/"
+for (const img of images) {
+    const newImg = document.createElement("img");
+    console.log(`img path: ${baseURL}${img.fileName}.jpg`)
+    newImg.setAttribute("src", `${baseURL}${img.fileName}.jpg`);
+    newImg.setAttribute("alt", `${img.alt}`);
+    newImg.setAttribute("tabIndex", "0");
+    thumbBar.appendChild(newImg);
+}
+
+
+// ON STEP 7!!
